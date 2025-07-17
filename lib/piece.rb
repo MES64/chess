69fe67b5c9b7898f564b213dfba343d6b7096de7
Mark Skelton <markskelton10@gmail.com>
2025-07-17 10:59:45 +0100

    feat: create #moveset_from method of Piece

--- lib/piece.rb
+++ lib/piece.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Piece appearance in terminal
+# Piece appearance on squares in terminal
 # 30 sets font color to black (otherwise it is white)
 # "\e[30;104m#{'♔ '}\e[0m" = White on Light
 # "\e[30;104m#{'♚ '}\e[0m" = Black on Light
@@ -10,10 +10,15 @@
 # Piece objects contain information about the chess piece and can generate the always-available
 # moves given a coordinate on the grid (assuming no boundaries on the board)
 class Piece
-  attr_reader :icon
+  attr_reader :moveset, :icon
 
-  def initialize(icon:)
+  def initialize(moveset: [], icon: '')
+    @moveset = moveset
     @icon = icon
+  end
+
+  def moveset_from(coordinate)
+    moveset.map { |move| [move[0] + coordinate[0], move[1] + coordinate[1]] }
   end
 
   def to_s
