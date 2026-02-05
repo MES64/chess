e7234892f1160b2b3d97c59d08abfc1c6e4295cf
Mark Skelton <markskelton10@gmail.com>
2026-02-05 17:05:14 +0000

    test: create #accept_draw? tests of HumanPlayer

--- spec/human_player_spec.rb
+++ spec/human_player_spec.rb
@@ -43,4 +43,34 @@ RSpec.describe HumanPlayer do
       expect(human_player_yes_no).to_not be_valid_yes_no(' hi')
     end
   end
+
+  describe '#accept_draw?' do
+    # Looping Script Method -> Test behavior of loop
+
+    subject(:human_player_accept) { described_class.new }
+
+    before do
+      allow(human_player_accept).to receive(:puts)
+      allow(human_player_accept).to receive(:user_input)
+      allow(human_player_accept).to receive(:draw_accepted?)
+    end
+
+    it 'does not loop when user input is a valid "y"/"n"' do
+      allow(human_player_accept).to receive(:valid_yes_no?).and_return(true)
+      expect(human_player_accept).to_not receive(:puts).with('Invalid Input!')
+      human_player_accept.accept_draw?(:white)
+    end
+
+    it 'loops once with an error message when user input is an invalid, then a valid "y"/"n"' do
+      allow(human_player_accept).to receive(:valid_yes_no?).and_return(false, true)
+      expect(human_player_accept).to receive(:puts).with('Invalid Input!').once
+      human_player_accept.accept_draw?(:black)
+    end
+
+    it 'loops 3 times each with an error message when user input is invalid, invalid, invalid, valid "y"/"n"' do
+      allow(human_player_accept).to receive(:valid_yes_no?).and_return(false, false, false, true)
+      expect(human_player_accept).to receive(:puts).with('Invalid Input!').exactly(3).times
+      human_player_accept.accept_draw?(:white)
+    end
+  end
 end
