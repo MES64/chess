3e78637819c9c3f8c1ce98a87c7158e90e2f3a0a
Mark Skelton <markskelton10@gmail.com>
2026-02-05 15:12:49 +0000

    test: create #draw_accepted? tests of HumanPlayer

---
+++ spec/human_player_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require_relative '../lib/human_player'
+
+RSpec.describe HumanPlayer do
+  # accept_draw? returns true/false depending on user input ('y'/'n')
+  # Looping Script; test behavior of loop
+  # Contains methods valid_yes_no? and draw_accepted?
+  # Note: puts/gets gets sent to self and passed up to Kernel
+
+  describe '#draw_accepted?' do
+    # Incoming Query Message -> Test value returned
+
+    subject(:human_player_draw) { described_class.new }
+
+    it 'returns true for input "y"' do
+      expect(human_player_draw).to be_draw_accepted('y')
+    end
+
+    it 'returns false for input "n"' do
+      expect(human_player_draw).to_not be_draw_accepted('n')
+    end
+  end
+end
