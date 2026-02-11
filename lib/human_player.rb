b82ff37f300e3df6cb555a17261f6c3eb42e6feb
Mark Skelton <markskelton10@gmail.com>
2026-02-11 17:39:23 +0000

    feat: create #make_move method of HumanPlayer

--- lib/human_player.rb
+++ lib/human_player.rb
@@ -4,6 +4,20 @@
 # - making a move with #make_move
 # - responding to a draw request with #accept_draw?
 class HumanPlayer
+  def make_move(game)
+    puts 'Enter a command'
+    loop do
+      done = send_to_game(game, user_input)
+      return if done
+
+      puts 'Enter a command again'
+    rescue NoMethodError
+      output_invalid_command
+    rescue ArgumentError
+      output_invalid_arguments
+    end
+  end
+
   def send_to_game(game, command)
     game.send(*command.split)
   end
@@ -30,5 +44,15 @@ class HumanPlayer
 
   def user_input
     gets.chomp
+  end
+
+  def output_invalid_command
+    puts 'Invalid Command!'
+    puts 'Enter a command again'
+  end
+
+  def output_invalid_arguments
+    puts 'Invalid Arguments!'
+    puts 'Enter a command again'
   end
 end
