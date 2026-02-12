3812be4a35f54e8840402919d7e5fa2b126ffe27
Mark Skelton <markskelton10@gmail.com>
2026-02-12 16:40:56 +0000

    test: create #copy_board tests of Game

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,6 +4,74 @@ require_relative '../lib/game'
 require_relative '../lib/board'
 
 RSpec.describe Game do
+  describe '#copy_board' do
+    # Outgoing Command Message -> Test message sent
+
+    let(:board_class) { instance_double('Class') }
+
+    before do
+      allow(board_class).to receive(:new)
+    end
+
+    context 'given a random board' do
+      let(:board) { instance_double('Board') }
+      let(:grid) do
+        [%w[a3 b3 c3],
+         %w[a2 b2 c2],
+         %w[a1 b1 c1]]
+      end
+      let(:castling) { { white: ['O-O', 'O-O-O'], black: ['O-O-O'] } }
+      let(:en_passant) { { white: ['move'], black: [] } }
+      let(:letter_to_piece) do
+        { white: { 'B' => 'WHITE_BISHOP', 'N' => 'WHITE_KNIGHT' },
+          black: { 'B' => 'BLACK_BISHOP', 'N' => 'BLACK_KNIGHT' } }
+      end
+
+      before do
+        allow(board).to receive(:grid).and_return(grid)
+        allow(board).to receive(:castling).and_return(castling)
+        allow(board).to receive(:en_passant).and_return(en_passant)
+        allow(board).to receive(:letter_to_piece).and_return(letter_to_piece)
+      end
+
+      subject(:game_copy) { described_class.new(board:, board_class:) }
+
+      it 'sends new to the Board class with the same instance variable values as the original board' do
+        expect(board_class).to receive(:new).with(grid:, castling:, en_passant:, letter_to_piece:)
+        game_copy.copy_board
+      end
+    end
+
+    context 'given a different random board' do
+      let(:board) { instance_double('Board') }
+      let(:grid) do
+        [%w[d3 e3 f3],
+         %w[d2 e2 f2],
+         %w[d1 e1 f1]]
+      end
+      let(:castling) { { white: ['O-O-O'], black: [] } }
+      let(:en_passant) { { white: [], black: [] } }
+      let(:letter_to_piece) do
+        { white: { 'R' => 'WHITE_ROOK', 'Q' => 'WHITE_QUEEN' },
+          black: { 'R' => 'BLACK_ROOK', 'Q' => 'BLACK_QUEEN' } }
+      end
+
+      before do
+        allow(board).to receive(:grid).and_return(grid)
+        allow(board).to receive(:castling).and_return(castling)
+        allow(board).to receive(:en_passant).and_return(en_passant)
+        allow(board).to receive(:letter_to_piece).and_return(letter_to_piece)
+      end
+
+      subject(:game_copy) { described_class.new(board:, board_class:) }
+
+      it 'sends new to the Board class with the same instance variable values as the original board' do
+        expect(board_class).to receive(:new).with(grid:, castling:, en_passant:, letter_to_piece:)
+        game_copy.copy_board
+      end
+    end
+  end
+
   describe '#update_moveset' do
     # Incoming Command Message -> Test change in observable state
 
