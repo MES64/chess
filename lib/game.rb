0dfd46631fc16f0765b28f0ef97e6e168fe12b35
Mark Skelton <markskelton10@gmail.com>
2026-02-12 17:29:23 +0000

    feat: create #update_given_board method of Game

--- lib/game.rb
+++ lib/game.rb
@@ -6,7 +6,7 @@
 class Game
   attr_reader :board, :board_class
 
-  def initialize(board:, board_class: nil)
+  def initialize(board: nil, board_class: nil)
     @board = board
     @board_class = board_class
   end
@@ -14,6 +14,10 @@ class Game
   def copy_board
     board_class.new(grid: board.grid, castling: board.castling, en_passant: board.en_passant,
                     letter_to_piece: board.letter_to_piece)
+  end
+
+  def update_given_board(given_board, move)
+    given_board.update(move)
   end
 
   def update_board(move)
