92d8dae1fddb0dff865fed80f52ff9203cd1c262
Mark Skelton <markskelton10@gmail.com>
2026-02-12 16:42:03 +0000

    feat: create Game with #copy_board method

---
+++ lib/game.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# Game contains all the info to have a game of chess: Board, Players, result, etc.
+# It contains methods to update the moveset, result, player turn, and board based on Player
+# commands: #move, #force_draw, #offer_draw, #resign, #save, #exit
+class Game
+  attr_reader :board, :board_class
+
+  def initialize(board:, board_class:)
+    @board = board
+    @board_class = board_class
+  end
+
+  def copy_board
+    board_class.new(grid: board.grid, castling: board.castling, en_passant: board.en_passant,
+                    letter_to_piece: board.letter_to_piece)
+  end
+end
