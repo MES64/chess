68759b1bcbda024c56ef3e21e84fa81cbea5b2a6
Mark Skelton <markskelton10@gmail.com>
2026-03-16 17:18:09 +0000

    feat: create the ComputerPlayer class
    At a turn count threshold, the computer player offers a draw, which
    another computer player always accepts. This prevents infinite loops.

---
+++ lib/computer_player.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+# ComputerPlayer decides the move to be taken by the PC, for:
+# - making a move with #make_move
+# - responding to a draw request with #accept_draw?
+class ComputerPlayer
+  attr_accessor :turn_count
+
+  def initialize
+    @turn_count = 0
+  end
+
+  def make_move(game)
+    if turn_count == 200
+      draw_accepted = send_offer_draw(game)
+      return if draw_accepted
+    end
+    send_move(game)
+    self.turn_count += 1
+  end
+
+  def accept_draw?(color)
+    puts "#{color}: Accept the draw? y/n"
+    puts 'y'
+    true
+  end
+
+  private
+
+  def send_offer_draw(game)
+    puts 'Computer Move'
+    puts 'offer_draw'
+    game.offer_draw
+  end
+
+  def send_move(game)
+    move = find_move(game)
+    puts 'Computer Move'
+    puts "move #{move}"
+    game.move(move)
+  end
+
+  def find_move(game)
+    my_color = game.current_player
+    my_moveset = game.moveset[my_color]
+    my_moveset.sample
+  end
+end
--- main.rb
+++ main.rb
@@ -3,11 +3,13 @@
 require_relative 'lib/game'
 require_relative 'lib/board'
 require_relative 'lib/human_player'
+require_relative 'lib/computer_player'
 
 board = Board.new
-player = HumanPlayer.new
+human_player = HumanPlayer.new
+computer_player = ComputerPlayer.new
 
-players = { white: player, black: player }
+players = { white: human_player, black: computer_player }
 moveset = { white: [], black: [] }
 
 game = Game.new(board:, moveset:, players:, current_player: :white, check: false, board_class: Board)
